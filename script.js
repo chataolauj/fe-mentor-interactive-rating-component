01c63b09bfdba3aeaee7da5f00876cb2b3935627
Toubee Lo <chataolauj@gmail.com>
2023-05-13 00:36:00 -0400

    Implemented submitting rating.

--- script.js
+++ script.js
@@ -11,13 +11,30 @@ function setRating(btn, ratingBtns) {
     btn.classList.add('active-rating');
 }
 
+function submitRating() {
+    let activeRating = document.querySelector('.active-rating');
+    let formView = document.querySelector('#form-view');
+    let confirmationView = document.querySelector('#confirmation-view');
+    let selectedRatingMessasge = document.querySelector('#selected-rating-message');
+
+    selectedRatingMessasge.innerHTML = 'You selected ' + activeRating.value + ' out of 5.'
+
+    formView.classList.add('hide-view');
+    confirmationView.classList.remove('hide-view');
+}
+
 function main() {
     let ratingBtns = Array.from(document.querySelectorAll('.rating-group > .circle-container'));
+    let submitBtn = document.querySelector('#submit-rating-btn');
 
     ratingBtns.forEach((btn) => {
         btn.addEventListener('click', (e) => {
             setRating(btn, ratingBtns);
         });
+    });
+
+    submitBtn.addEventListener('click', (e) => {
+        submitRating()
     });
 }
 
